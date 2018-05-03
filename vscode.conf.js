50788cce1480e820e9ad0c5d1c30d766ee73510f
hanipcode <hanif@shwetech.com>
2018-05-03 10:26:17 +0700

    add vscode

---
+++ vscode.conf.js
@@ -0,0 +1,25 @@
+{
+    "workbench.colorTheme": "Nord",
+    "workbench.iconTheme": "vscode-icons",
+    "editor.fontFamily": "'Fira Code'",
+    "editor.fontLigatures": true,
+    "editor.fontWeight": "500",
+    "editor.formatOnSave": true,
+    "tslint.autoFixOnSave": true,
+    "flow.runOnAllFiles": false,
+    "prettier.eslintIntegration": true,
+    "javascript.format.enable": true,
+    "javascript.validate.enable": true,
+    "sqltools.telemetryUUID": "2f36c6dd-0865-48f7-bb95-4f0626af8a2f",
+    "python.disablePromptForFeatures": [
+        "pylint"
+    ],
+    "editor.insertSpaces": true,
+    "prettier.tabWidth": 2,
+    "editor.tabSize": 2,
+    "prettier.singleQuote": true,
+    "editor.tabCompletion": true,
+    "editor.autoClosingBrackets": true,
+    "window.zoomLevel": 0,
+    "editor.minimap.enabled": false,
+}
